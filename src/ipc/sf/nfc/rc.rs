fa87f6b5fb9dc829df41f50c0b6b2c3994a4df54
Joshua de Reeper <joshua@dereeper.co.nz>
2025-05-19 02:14:16 +0200

    nfc result codes

---
+++ src/ipc/sf/nfc/rc.rs
@@ -0,0 +1,10 @@
+pub const RESULT_MODULE: u32 = 161;
+
+result_define_group!(RESULT_MODULE => {
+    DeviceNotFound: 64,
+    WrongArgument: 65,
+    WrongDeviceState: 73,
+    NFCDisabled: 80,
+    TagNotFound: 97,
+    MifareAccessError: 288,
+});
